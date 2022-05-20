ca818daf8f9ad30aa0f85c9a69a59d2fca62af13
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-05-19 22:15:26 -0300

    programa para banho e tosa

--- src/Funcoes.java
+++ src/Funcoes.java
@@ -1,15 +1,23 @@
+import javax.swing.JOptionPane;
 
 public class Funcoes {
 	public static void main(String[] args) {
-		soma(10, 20);
-		mult(10, 200);
+		// calcular gasto com banho do Biel
+
+		taxa(Integer.parseInt(JOptionPane.showInputDialog("Loja 1: ")),
+				Integer.parseInt(JOptionPane.showInputDialog("Loja 2: ")));
+
 	}
 
-	static void soma(int x, int y) {
-		System.out.println(x + y);
+	static void taxa(double a, double b) {
+		if (a < b) {
+			System.out.printf("Escolher a Loja1, valor do desconto é R$ " + (a / 100) * 10);
+		} else if (b < a) {
+			System.out.printf("Escolher a Loja2, valor do desconto é R$ " + (b / 20) * 10);
+		} else {
+			System.out.println("Escolher a mais próxima de casa!");
+		}
+		System.out.println();
 	}
 
-	static void mult(int a, int b) {
-		System.out.println(a * b);
-	}
 }
