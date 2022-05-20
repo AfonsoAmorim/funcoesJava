1a9705a3bb41a260424fdd835433b193ff142912
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-05-19 21:30:26 -0300

    teste de funções

--- src/Funcoes.java
+++ src/Funcoes.java
@@ -1,10 +1,15 @@
 
 public class Funcoes {
 	public static void main(String[] args) {
-		
+		soma(10, 20);
+		mult(10, 200);
 	}
-	
-	static void sum() {
-		
+
+	static void soma(int x, int y) {
+		System.out.println(x + y);
+	}
+
+	static void mult(int a, int b) {
+		System.out.println(a * b);
 	}
 }
